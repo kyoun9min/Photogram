cb08f4cbd09b9c477b4f40a8c9c31ef792f3e0d4
kyoun9min <rtong77@naver.com>
2025-12-23 13:23:37 +0900

    프로필 페이지 - 이미지 렌더링 준비

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.domain.user;
 
+import com.cos.photogramstart.domain.image.Image;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -42,6 +44,13 @@ public class User {
     private String profileImageUrl; // 사진
 
     private String role; // 권한
+
+    //나는 연관관계의 주인이 아니다. 그러므로 테이블에 컬럼을 만들지마.
+    // User를 Select할 때 해당 User id로 등록된 image들을 다 가져와
+    // Lazy = User를 Select할 때 해당 User id로 등록된 image들을 가져오지마 - 대신 getImages() 함수의 image들이 호출될 때 가져와!!
+    // Eager = User를 Select할 때 해당 User id로 등록된 image들을 전부 Join해서 가져와!!
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Image> images; // 양방향 매핑
 
     private LocalDateTime createDate;
 
