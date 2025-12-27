a9fe31b2a987faea1fbffebe583adfcefcc5feba
kyoun9min <rtong77@naver.com>
2025-12-28 00:12:59 +0900

    구독 화면 - 뷰 렌더링 2

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -3,15 +3,26 @@ package com.cos.photogramstart.service;
 import com.cos.photogramstart.domain.subscribe.Subscribe;
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SubscribeService {
 
     private final SubscribeRepository subscribeRepository;
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> 구독리스트(int principalId, int pageUserId) {
+
+
+
+        return null;
+    }
 
     @Transactional
     public void 구독하기(int fromUserId, int toUserId) {
