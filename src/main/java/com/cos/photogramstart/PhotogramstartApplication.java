db4fbea63513579f8002c6add9f930dd8c17b8f4
kyoun9min <rtong77@naver.com>
2026-02-02 16:01:27 +0900

    feat: Redis 세션 관리 도입 및 WAS 컨테이너 2대 확장 (롤링 배포)

--- src/main/java/com/cos/photogramstart/PhotogramstartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramstartApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession // 이 어노테이션이 Redis 세션 관리를 활성화
 @SpringBootApplication
 public class PhotogramstartApplication extends SpringBootServletInitializer {
 
